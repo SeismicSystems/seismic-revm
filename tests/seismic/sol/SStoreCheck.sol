3ad5dfeb097d6a3508600c17fe53251c5adda194
alex-lindenbaum <a@seismic.systems>
2024-09-19 14:20:04 -0400

    sstore kstore tests

---
+++ tests/seismic/sol/SStoreCheck.sol
@@ -0,0 +1,24 @@
+contract SStoreCheck {
+    function test() public {
+        uint256 slot = 0x987654321;
+        uint256 otherSlot = 0x1111;
+
+        uint256 emptyResult;
+        assembly {
+            emptyResult := sload(slot)
+        }
+
+        uint256 expected = 0x123456789;
+        uint256 result;
+        uint256 otherResult;
+        assembly {
+            sstore(slot, expected)
+            result := sload(slot)
+            otherResult := sload(otherSlot)
+        }
+
+        assert(emptyResult == 0);
+        assert(result == expected);
+        assert(otherResult == 0);
+    }
+}
