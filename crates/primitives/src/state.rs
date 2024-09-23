3b73ba7f9ee5840a24f837430fd9b3e5bdfb5d0b
phexyz <32248504+phexyz@users.noreply.github.com>
2024-09-23 13:10:52 -0400

    trying to derive arbitrary

--- crates/primitives/src/state.rs
+++ crates/primitives/src/state.rs
@@ -1,4 +1,6 @@
 use crate::{Address, Bytecode, HashMap, SpecId, B256, KECCAK_EMPTY, U256};
+use arbitrary::Arbitrary;
+use alloy_primitives::Uint;
 use crate::ruint::UintTryFrom;
 use alloy_primitives::FixedBytes;
 use bitflags::bitflags;
@@ -171,6 +173,15 @@ impl From<AccountInfo> for Account {
 pub struct FlaggedStorage {
     pub value: U256,
     pub is_private: bool,
+}
+
+
+impl arbitrary::Arbitrary<'_> for FlaggedStorage {
+    fn arbitrary(u: &mut arbitrary::Unstructured<'_>) -> arbitrary::Result<Self> {
+        let arbitrary_u256 = u.arbitrary::<Uint<256, 4>>()?;
+        let arbitrary_bool = u.arbitrary::<bool>()?;
+        Ok(FlaggedStorage::new_from_tuple((arbitrary_u256, arbitrary_bool)))
+    }
 }
 
 impl From<FlaggedStorage> for FixedBytes<32> {
