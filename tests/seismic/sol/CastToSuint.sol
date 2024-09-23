8c9932f4fb4cf1105c69fc6a594a944a68fef8f9
alex-lindenbaum <a@seismic.systems>
2024-09-23 18:46:08 -0400

    test implicit/explicit casting

--- tests/seismic/sol/CastToSuint.sol
+++ tests/seismic/sol/CastToSuint.sol
@@ -1,9 +1,11 @@
 contract CastToSuint {
-    function test() public {
-        uint256 x = 0x123456789;
-        suint256 y = 0x123456789;
-        suint256 z = x;
+    suint256 z;
+    suint256 x;
 
-        assert(y == z);
+    function test() public {
+        // uint256 x = 0x123456789;
+        // suint256 y = 0x123456789;
+        z = 0x555;
+        x = z;
     }
 }
