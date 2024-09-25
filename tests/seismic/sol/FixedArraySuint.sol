302f308a7cca85853e830c09232ce7f4daaf1933
alex-lindenbaum <a@seismic.systems>
2024-09-25 15:50:55 -0400

    rm

--- tests/seismic/sol/FixedArraySuint.sol
+++ tests/seismic/sol/FixedArraySuint.sol
@@ -1,11 +1,6 @@
 contract FixedArraySuint {
     uint256[10] arr;
 
-    uint256 public x;
-    uint256 internal y;
-    uint256 private z;
-
-
     function test() public {
         for (uint256 i = 0; i < 10; i++) {
             arr[i] = 2 ** i;
