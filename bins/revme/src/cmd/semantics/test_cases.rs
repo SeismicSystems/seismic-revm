428027bca67e725670f1809d042d3941d4df8537
SFYLL <sd@seismic.systems>
2024-10-16 13:59:55 +0200

    further workinng constructor

--- bins/revme/src/cmd/semantics/test_cases.rs
+++ bins/revme/src/cmd/semantics/test_cases.rs
@@ -1,15 +1,19 @@
+use std::str::FromStr;
+
 use super::{errors::Errors, semantic_tests::ContractInfo, parser::Parser};
-use revm::primitives::{Bytes};
+use alloy_primitives::U256;
+use revm::primitives::Bytes;
 
-const SKIP_KEYWORD: [&str; 5] = ["gas", "wei", "emit", "Library", "FAILURE"];
+const SKIP_KEYWORD: [&str; 4] = ["gas", "emit", "Library", "FAILURE"];
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub(crate) struct TestCase {
     pub function_name: String,
     pub input_data: Bytes,
     pub expected_outputs: Bytes,
     pub is_constructor: bool,
     pub deploy_binary: Bytes,
+    pub value: U256,
 }
 
 impl TestCase {
@@ -28,6 +32,7 @@ impl TestCase {
             };
 
             // Example: "add(uint256,uint256): 1,2 -> 3"
+            // constructor(), 2 wei: 3 ->"
             let parts: Vec<&str> = line.split("->").collect();
             if parts.len() < 2  || parts.len() > 3 {
                 continue;
@@ -36,31 +41,41 @@ impl TestCase {
             let should_skip = SKIP_KEYWORD.iter().any(|&keyword| {
                 parts.iter().any(|&part| part.contains(keyword))
             });
-
+            
             if should_skip {
                 continue; 
             }
 
             let call_part = parts[0].trim();
             let expected_output_part = parts[1].trim();
-
-            let signature_and_args: Vec<&str> = call_part.split(':').collect();
-            if signature_and_args.len() > 2 {
-                continue;
-            }
             
-            let mut is_constructor = false;
+            let mut value = U256::ZERO;
+            let signature_and_args_temp: Vec<&str> = call_part.split(':').collect();
+            let signature: Vec<_> = signature_and_args_temp[0].trim().split(",").collect();
+            let mut signature_and_args: Vec<&str> = Vec::new();
 
-            if signature_and_args[0].starts_with("constructor(") {
-                is_constructor = true
+            if signature.len() == 2 {
+                let string_value = signature[1].trim().split(" ").collect::<Vec<_>>()[0];
+                value = U256::from_str(string_value).map_err(|_| Errors::InvalidInput)?;
+                signature_and_args.push(signature[0]);
+                signature_and_args.push(signature_and_args_temp[1]);
             }
-            
+            else {
+                signature_and_args = signature_and_args_temp;
+            }
+
             let (function_selector, parameter_types) = Parser::parse_function_signature(signature_and_args[0].trim())?;
+            
             let args_list = if signature_and_args.len() > 1 && !signature_and_args[1].trim().is_empty() {
                 signature_and_args[1].trim().split(',').map(|arg| arg.trim()).collect::<Vec<&str>>()
             } else {
                 Vec::new() 
             };
+            
+            let mut is_constructor = false;
+            if signature_and_args[0].starts_with("constructor(") {
+                is_constructor = true
+            }
 
             if args_list.len() != parameter_types.len() {
                 return Err(Errors::InvalidArgumentCount);
@@ -74,7 +89,7 @@ impl TestCase {
 
             let mut input_data = Vec::new();
             input_data.extend_from_slice(&function_selector);
-            for arg in args_encoded {
+            for arg in args_encoded.clone() {
                 input_data.extend_from_slice(&arg);
             }
 
@@ -91,13 +106,30 @@ impl TestCase {
             });
 
             if let Some(contract) = matching_contract {
-                test_cases.push(TestCase {
-                    function_name: signature_and_args[0].to_string(), 
-                    input_data: input_data.into(), 
-                    expected_outputs: Bytes::from(expected_outputs),
-                    is_constructor,
-                    deploy_binary: contract.compile_binary.clone(),  
-                });
+                let mut deploy_binary = Vec::new();
+                deploy_binary.extend_from_slice(&contract.compile_binary);
+                if is_constructor {
+                    for arg in args_encoded {
+                            deploy_binary.extend_from_slice(&arg);
+                    }
+                        test_cases.push(TestCase {
+                            function_name: signature_and_args[0].to_string(), 
+                            input_data: vec![].into(), 
+                            expected_outputs: Bytes::from(expected_outputs),
+                            is_constructor,
+                            deploy_binary: deploy_binary.into(),  
+                            value,  
+                    });
+                } else {
+                    test_cases.push(TestCase {
+                        function_name: signature_and_args[0].to_string(), 
+                        input_data: input_data.into(), 
+                        expected_outputs: Bytes::from(expected_outputs),
+                        is_constructor,
+                        deploy_binary: deploy_binary.into(),  
+                        value,  
+                    });
+                }
             } else {
                 // Handle case where no matching contract is found (optional)
                 println!("No matching contract found for function: {}", signature_and_args[0].trim());
