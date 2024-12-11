9ddb5c6bdf26296e49d8fce55e5afefba7eadd69
SFYLL <sd@seismic.systems>
2024-12-11 14:33:24 +0100

    seismic handler

---
+++ crates/revm/src/seismic.rs
@@ -0,0 +1,5 @@
+//! Seismic specific precompiles and handler_register.
+
+mod handler_register;
+
+pub use handler_register::{seismic_handle_register, load_precompiles};
---
+++ crates/revm/src/seismic/handler_register.rs
@@ -0,0 +1,32 @@
+//! Handler related to Optimism chain
+
+use crate::{
+    handler::register::EvmHandler,
+    primitives::{
+        db::Database, spec_to_generic, Spec, SpecId
+    },
+    ContextPrecompiles
+};
+use revm_precompile::{secp256r1, PrecompileSpecId};
+use std::sync::Arc;
+
+pub fn seismic_handle_register<DB: Database, EXT>(handler: &mut EvmHandler<'_, EXT, DB>) {
+    spec_to_generic!(handler.cfg.spec_id, {
+        // validate environment
+        handler.pre_execution.load_precompiles = Arc::new(load_precompiles::<SPEC, EXT, DB>);
+    });
+}
+// Load precompiles for Optimism chain.
+#[inline]
+pub fn load_precompiles<SPEC: Spec, EXT, DB: Database>() -> ContextPrecompiles<DB> {
+    let mut precompiles = ContextPrecompiles::new(PrecompileSpecId::from_spec_id(SPEC::SPEC_ID));
+
+    if SPEC::enabled(SpecId::MERCURY) {
+        precompiles.extend([
+            // EIP-7212: secp256r1 P256verify
+            secp256r1::P256VERIFY,
+        ])
+    }
+    precompiles
+}
+
