c89ae5966a5e8d3fe7d563fa6191cb6298eca599
SFYLL <sd@seismic.systems>
2024-12-11 15:06:33 +0100

    latest WIP

--- crates/revm/src/seismic/rng.rs
+++ crates/revm/src/seismic/rng.rs
@@ -1,1 +1,9 @@
-//TODO
+use revm_precompile::{u64_to_address,  Precompile, PrecompileOutput, PrecompileResult, PrecompileWithAddress};
+use crate::primitives::{Env, Bytes};
+
+pub const RNG: PrecompileWithAddress=
+    PrecompileWithAddress(u64_to_address(100), Precompile::Env(run));
+
+pub fn run(input: &Bytes, gas_limit: u64, env: &Env) -> PrecompileResult {
+    Ok(PrecompileOutput::new(10, Bytes::new()))
+}
