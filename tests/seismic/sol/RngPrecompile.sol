eedc237a5bf155db1fdc6984164dae8bab21c04e
SFYLL <sd@seismic.systems>
2024-12-13 09:57:38 +0100

    clean test further

--- tests/seismic/sol/RngPrecompile.sol
+++ tests/seismic/sol/RngPrecompile.sol
@@ -34,17 +34,11 @@ contract SEISMICRNG {
         }
         
     }
-
-    function test_seismicRngPers(bytes32 pers) public view returns (bytes32 result) {
-        result = seismicRngPers(pers);
-    }
-
 }
-// TODO: figure out how to test seismicRngPers() better, remove test_seismicRngPers()
 // ====
 // EVMVersion: >=mercury
 // ====
 // ----
 // seismicRng() -> 0x7a40ad457b8ccfc0f97d740a2fc28a7a0eb2ab0cb1934dedb53c0d8b2139e897
 // seismicRng() -> 0x19cfb3ebd326b1a312640e740f7e2eb70b192daccfeaef1979bb012d0b1d6507
-// test_seismicRngPers(bytes32): 0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef ->  0x4ceef969af9666a582ddbf3a898a6b9e5a38947152b241defd9a852f4fc5697b
+// seismicRngPers(bytes32): 0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef ->  0x4ceef969af9666a582ddbf3a898a6b9e5a38947152b241defd9a852f4fc5697b
