642bc50c6d37cf8f7a0a9029c3daf5d681e772ab
Spencer Solit <sol@seismic.systems>
2024-12-16 12:41:46 -0500

    import existing aes functions, mapped out how precompile will look

--- crates/revm/src/seismic/eph_key/secp256k1_eph.rs
+++ crates/revm/src/seismic/eph_key/secp256k1_eph.rs
@@ -5,12 +5,21 @@ use secp256k1::{
     ecdsa::{RecoverableSignature, RecoveryId},
     Message, SECP256K1,
     generate_keypair,
+    SecretKey,
+    PublicKey,
+    ecdh::SharedSecret,
 };
 use revm_precompile::{
     u64_to_address, Error as REVM_ERROR, PrecompileError, PrecompileOutput, PrecompileResult,
 };
 use crate::seismic::rng::precompile::get_leaf_rng;
 use crate::precompile::Error as PCError;
+use aes_gcm::{
+    aead::{generic_array::GenericArray, Aead, AeadCore, KeyInit, OsRng as AesRng},
+    Aes256Gcm, 
+    Key
+};
+use tee_service_api::{aes_decrypt, aes_encrypt, derive_aes_key};
 
 pub fn gen_secp256k1_sk<DB: Database>(
     input: &Bytes,
@@ -31,36 +40,40 @@ pub fn gen_secp256k1_sk<DB: Database>(
     Ok(PrecompileOutput::new(gas_used, sk_bytes.into()))
 }
 
+pub fn derive_symmetric_key(input: &Bytes, gas_limit: u64) -> PrecompileResult {
+    // Process the input
+    // if input.len() != INPUT_LENGTH {
+    //     return Err(Error::Blake2WrongLength.into());
+    // }
+    let sk_bytes = &input[0..32];
+    let pk_bytes = &input[32..64];
+    let sk = SecretKey::from_slice(sk_bytes).map_err(|_| PrecompileError::Other("invalid sk".to_string()))?;
+    let pk = PublicKey::from_slice(pk_bytes).map_err(|_| PrecompileError::Other("invalid pk".to_string()))?;
 
-// fn rng_call<DB: Database>(
-//     input: &Bytes,
-//     gas_limit: u64,
-//     evmctx: &mut InnerEvmContext<DB>,
-// ) -> Result<Bytes, PrecompileError> {
-//         let gas_used = 100; // TODO: refine this constant
-//         if gas_used > gas_limit {
-//             return Err(REVM_ERROR::OutOfGas.into());
-//         }
+    // derive the shared secret
+    let shared_secret = SharedSecret::new( &pk, &sk);
+    // derive the AES key
+    let aes_key = derive_aes_key(&shared_secret).unwrap(); // TODO: no unwraps 
+    let output = aes_key.to_vec(); // TODO: coerce this to be a specific size
 
-//         let pers = input.as_ref(); // pers is the personalized entropy added by the caller
+    Ok(PrecompileOutput::new(gas_limit, output.into()))
+}
 
-//         // Get the random bytes
-//         // TODO: evaluate if this is good, ex if the tx_hash is correct
-//         let env = evmctx.env().clone();
-//         let root_rng = &mut evmctx.kernel.root_rng;
-//         let tx_hash = hash_tx_env(&env.tx);
-//         root_rng.append_tx(tx_hash);
-//         let mut leaf_rng = match root_rng.fork(&env, pers) {
-//             Ok(rng) => rng,
-//             Err(_err) => {
-//                 return Err(PrecompileError::Other("Rng fork failed".to_string()).into());
-//             }
-//         };
-
-//         let mut rng_bytes = [0u8; 32];
-//         leaf_rng.fill_bytes(&mut rng_bytes);
-//         let output = Bytes::from(rng_bytes);
-
-//         Ok(output)
-//     }
+pub fn encrypt_event(input: &Bytes, gas_limit: u64) -> PrecompileResult {
+    // Process the input
+    // if input.len() != INPUT_LENGTH {
+    //     return Err(Error::Blake2WrongLength.into());
+    // }
+    let aes_key = Key::<Aes256Gcm>::from_slice(&input[0..32]);
+    // Interpret bytes as a big-endian `u64`
+    let nonce_bytes: [u8; 8] = input[0..32].try_into().unwrap();
+    let nonce_be: u64 = u64::from_be_bytes(nonce_bytes);
+    let plaintext = input[40..].to_vec();
+ 
+    // encrypt the plaintext
+    let ciphertext = aes_encrypt(&aes_key, &plaintext, nonce_be).unwrap(); // TODO: no unwraps
     
+    // prepare the output: (nonce, ciphertext + authtag)
+    
+    Ok(PrecompileOutput::new(gas_limit, ciphertext.into()))
+}
