e5712c396cf94efcc9a3343c3b43df7d3441d422
Spencer Solit <sol@seismic.systems>
2024-12-17 11:39:16 -0500

    test contract for gen secp256k1 key

---
+++ tests/seismic/sol/GenSecp256k1.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract GENSECP256K1 {
+    function genKey(bytes32 rngPers) public view returns (bytes memory) {
+        // require(false, "hardcode fail");
+        address gen_precompile = address(0x65);
+
+        bytes memory input = bytes.concat(rngPers);
+
+        // Call the precompile
+        (bool success, bytes memory output) = gen_precompile.staticcall(input);
+        // Ensure the call was successful
+        require(success, "GenSecp256k1KeysPrecompile call failed");
+
+        assembly {
+            let len := mload(output)
+            let data := add(output, 32)
+            return(data, len)
+        }
+    }
+}
+// ====
+// EVMVersion: >=mercury
+// ====
+// ----
+// genKey(bytes32): 0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef ->  0x7045d680ba85139d5338eca1d5aa9b3f1da3992024b466cd0afb2fedce3188bb
