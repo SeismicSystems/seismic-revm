b5d86294a7cc3317de340038eac24538027347d6
Spencer Solit <sol@seismic.systems>
2024-12-17 12:41:28 -0500

    deriveAESKey.sol takes in bytes32 for sk

--- tests/seismic/sol/DeriveAESKey.sol
+++ tests/seismic/sol/DeriveAESKey.sol
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// TODO: make sk a bytes32?
-
 contract DERIVEAESKEY {
     /// @notice Derives an AES key using a precompiled contract.
     /// @param sk The secret key as 32 bytes.
     /// @param pk The public key as 33 bytes.
     /// @return result The derived AES key as bytes32.
-    function deriveAESKey(bytes memory sk, bytes memory pk) public view returns (bytes32 result) {
+    function deriveAESKey(bytes32 sk, bytes memory pk) public view returns (bytes32 result) {
         // Address of the precompiled contract
         address deriveAESKeyPrecompile = address(0x66);
 
@@ -30,9 +28,9 @@ contract DERIVEAESKEY {
 
     function testDeriveAESKey() public view {
         // Define the secret keys (sk) and public keys (pk) as hexadecimal literals
-        bytes memory sk1 = hex"7e38022030c40773cc561c1cc9c0053e48b0be2cee33c13495f096942ea176ef";
+        bytes32 sk1 = hex"7e38022030c40773cc561c1cc9c0053e48b0be2cee33c13495f096942ea176ef";
         bytes memory pk1 = hex"03f176e697b5b0c4799f1816f5fe114263d1c01a84ad296129f994278499f0842e";
-        bytes memory sk2 = hex"adbed354135e517bc881d55fa60c455737d1ba98d446c0866cec3837e13d9906";
+        bytes32 sk2 = hex"adbed354135e517bc881d55fa60c455737d1ba98d446c0866cec3837e13d9906";
         bytes memory pk2 = hex"02555d7b94d8afc4afdf5a03e9da73a408b6d19c865036bae833864d2353e85a25";
 
         // Create inputs by concatenating secret keys and public keys
