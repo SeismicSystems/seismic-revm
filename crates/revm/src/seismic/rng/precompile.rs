b0bfc60d3835832b43c28bce5b858c0825e6fd7d
Spencer Solit <sol@seismic.systems>
2025-01-20 13:01:20 -0500

    first shot at rng pricing

--- crates/revm/src/seismic/rng/precompile.rs
+++ crates/revm/src/seismic/rng/precompile.rs
@@ -47,7 +47,37 @@ Precompile Logic
 /// it for bytes.
 /// 
 /// ## Gas Cost
+/// ### Pricing Fundamental Operations 
+/// The RNG precompile uses Merlin transcripts that rely on the Strobe128 hash function.
+/// Strobe uses the keccak256 sponge, which has an evm opcode cost of 
+/// g=30+6×ceil(input size/32). Strobe128 has a more complex initialization than SHA,
+/// so we price a base cost of 100 gas plus 6×ceil(input size/32). 
+/// The transcripts also use points on the Ristretto group for Curve25519, and require
+/// scalar multiplications. These EC operations are 2-3x more efficient than BLS12-381 
+/// operations, so we price it at g = 6000, half the cost of a BLS12-381 G1 operation.
 /// 
+/// ### Pricing RNG Operations
+/// The cost of the RNG comes from the following:
+/// * The RNG initialization requires a running hash of the transcript using strobe128.
+/// where a 32 byte tx_hash and label 2 bytes are added per transaction. 
+/// * A seperate VRF Hash function that performs a single EC scalar multiplication 
+/// is used whenever the RNG is forked as domain seperation
+/// * The Root rng is initialized, which involves adding 13 bytes to the transcript 
+/// and then keying the rng (essentially hashing)
+/// * The leaf RNG is initialized, which involves keying the rng based on 32 random bytes
+/// from the parent RNG. 
+/// * Filling bytes once the rng is initialized. This requires the squeeze operation,
+/// which is just copying bytes since we currently restring the rng request to 32 bytes
+/// 
+/// This is 100 gas from setting up Strobe128
+/// 79 bytes of hashing to initialize the RNG. 79 * 6 = 474 gas
+/// 6000 gas for the EC scalar multiplication
+/// We add a 50 percent buffer to our gas calculations, which may be lowered in the future
+/// (100 + 474 + 6000) * 1.25 = 9800 gas
+/// 
+/// TODO: add cost of pers bytes. Perhaps force this to be a bytes32 if used?
+/// TODO: add a way to request a longer output than 32 bytes for efficiency
+/// TODO: TBD if root rng needs to be initialized for every transaction
 impl<DB: Database> ContextStatefulPrecompile<DB> for RngPrecompile {
     fn call(
         &self,
@@ -55,7 +85,7 @@ impl<DB: Database> ContextStatefulPrecompile<DB> for RngPrecompile {
         gas_limit: u64,
         evmctx: &mut InnerEvmContext<DB>,
     ) -> PrecompileResult {
-        let gas_used = 100; // TODO: refine this constant
+        let gas_used = 9800;
         if gas_used > gas_limit {
             return Err(REVM_ERROR::OutOfGas.into());
         }
@@ -63,7 +93,6 @@ impl<DB: Database> ContextStatefulPrecompile<DB> for RngPrecompile {
         // Get the random bytes
         let mut leaf_rng =
             get_leaf_rng(input, evmctx).map_err(|e| PCError::Other(e.to_string()))?;
-
         let mut rng_bytes = [0u8; 32];
         leaf_rng.fill_bytes(&mut rng_bytes);
         let output = Bytes::from(rng_bytes);
