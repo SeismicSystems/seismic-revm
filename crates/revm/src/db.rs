94040cc04c74549bcf45a011c4a156a120fc7738
sfyll <39958632+sfyll@users.noreply.github.com>
2025-02-11 11:23:41 +0100

    Update cargo (#78)

--- crates/revm/src/db.rs
+++ crates/revm/src/db.rs
@@ -1,11 +1,22 @@
 //! [Database] implementations.
 
+#[cfg(any(feature = "alloydb", feature = "ethersdb"))]
+mod utils;
+
+#[cfg(feature = "alloydb")]
+mod alloydb;
 pub mod emptydb;
+#[cfg(feature = "ethersdb")]
+mod ethersdb;
 pub mod in_memory_db;
 pub mod states;
 
 pub use crate::primitives::db::*;
+#[cfg(feature = "alloydb")]
+pub use alloydb::AlloyDB;
 pub use emptydb::{EmptyDB, EmptyDBTyped};
+#[cfg(feature = "ethersdb")]
+pub use ethersdb::EthersDB;
 pub use in_memory_db::*;
 pub use states::{
     AccountRevert, AccountStatus, BundleAccount, BundleState, CacheState, DBBox,
