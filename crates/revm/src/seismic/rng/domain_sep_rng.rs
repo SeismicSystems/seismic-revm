30f3e8200c6c573418f7413286f7b185f2acceca
cdrappi <c@seismic.systems>
2025-02-11 17:20:46 -0500

    rename TEEService to enclave

--- crates/revm/src/seismic/rng/domain_sep_rng.rs
+++ crates/revm/src/seismic/rng/domain_sep_rng.rs
@@ -11,7 +11,7 @@ use merlin::{Transcript, TranscriptRng};
 use rand_core::{CryptoRng, OsRng, RngCore};
 pub use schnorrkel::keys::Keypair as SchnorrkelKeypair;
 use std::{cell::RefCell, rc::Rc};
-use tee_service_api::get_sample_schnorrkel_keypair;
+use seismic_enclave::get_sample_schnorrkel_keypair;
 
 /// RNG domain separation context.
 const RNG_CONTEXT: &[u8] = b"seismic rng context";
