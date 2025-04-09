c0c5a0a4a0118c4eb2eee68b6e33b066ff3f2c15
SFYLL <sd@seismic.systems>
2025-04-09 14:06:54 +0200

    clean up handler/had rng hook

--- crates/seismic/src/handler.rs
+++ crates/seismic/src/handler.rs
@@ -1,8 +1,8 @@
 //!Handler related to Seismic chain
 use crate::api::exec::SeismicContextTr;
 use revm::{
-    context::result::{HaltReason, InvalidTransaction}, context_interface::
-        result::{EVMError, FromStringError, ResultAndState},
+    context::{result::{HaltReason, InvalidTransaction}, ContextTr}, context_interface::
+        result::{EVMError, FromStringError},
     handler::{
         handler::EvmTrError, EvmTr, Frame, FrameResult,
         Handler, MainnetHandler,
@@ -51,65 +51,9 @@ where
     type Frame = FRAME;
     type HaltReason = HaltReason;
 
-    fn validate_env(&self, evm: &mut Self::Evm) -> Result<(), Self::Error> {
-        self.mainnet.validate_env(evm)
-    }
-
-    fn validate_tx_against_state(&self, evm: &mut Self::Evm) -> Result<(), Self::Error> {
-        self.mainnet.validate_tx_against_state(evm)
-    }
-
-    fn deduct_caller(&self, evm: &mut Self::Evm) -> Result<(), Self::Error> {
-        self.mainnet.deduct_caller(evm)
-    }
-
-    fn last_frame_result(
-        &self,
-        evm: &mut Self::Evm,
-        frame_result: &mut <Self::Frame as Frame>::FrameResult,
-    ) -> Result<(), Self::Error> {
-        self.mainnet.last_frame_result(evm, frame_result)
-    }
-
-    fn reimburse_caller(
-        &self,
-        evm: &mut Self::Evm,
-        exec_result: &mut <Self::Frame as Frame>::FrameResult,
-    ) -> Result<(), Self::Error> {
-        self.mainnet.reimburse_caller(evm, exec_result)
-    }
-
-    fn refund(
-        &self,
-        evm: &mut Self::Evm,
-        exec_result: &mut <Self::Frame as Frame>::FrameResult,
-        eip7702_refund: i64,
-    ) {
-        self.mainnet.refund(evm, exec_result, eip7702_refund)
-    }
-
-    fn reward_beneficiary(
-        &self,
-        evm: &mut Self::Evm,
-        exec_result: &mut <Self::Frame as Frame>::FrameResult,
-    ) -> Result<(), Self::Error> {
-        self.mainnet.reward_beneficiary(evm, exec_result)
-    }
-
-    fn output(
-        &self,
-        evm: &mut Self::Evm,
-        result: <Self::Frame as Frame>::FrameResult,
-    ) -> Result<ResultAndState<Self::HaltReason>, Self::Error> {
-        self.mainnet.output(evm, result)
-    }
-
-    fn catch_error(
-        &self,
-        evm: &mut Self::Evm,
-        error: Self::Error,
-    ) -> Result<ResultAndState<Self::HaltReason>, Self::Error> {
-        self.mainnet.catch_error(evm, error) 
+    fn pre_execution(&self, evm: &mut Self::Evm) -> Result<u64, Self::Error> {
+        evm.ctx().chain().reset_rng();
+        self.mainnet.pre_execution(evm)
     }
 }
 
