0cac97ca63922720434b4d46d2eec51e27372b7e
SFYLL <sd@seismic.systems>
2025-04-15 15:02:04 +0200

    call inner

--- crates/seismic/src/instructions/seismic_host.rs
+++ crates/seismic/src/instructions/seismic_host.rs
@@ -1,7 +1,7 @@
 use revm::{
     context::ContextTr, context_interface::{
         context::ContextError, journaled_state::AccountLoad, Database
-    }, database::EmptyDB, interpreter::{Host, SStoreResult, SelfDestructResult, StateLoad}, primitives::{Address, Bytes, Log, B256, U256}
+    }, database::EmptyDB, interpreter::{host::DummyHost, Host, SStoreResult, SelfDestructResult, StateLoad}, primitives::{Address, Bytes, Log, B256, U256}
 };
 
 use crate::api::exec::SeismicContextTr;
@@ -26,12 +26,14 @@ where
 
 pub struct SeismicDummyHost {
     ctx_result: Result<(), ContextError<<EmptyDB as Database>::Error>>,
+    dummy_host: DummyHost,
 }
 
 impl SeismicDummyHost {
     pub fn new() -> Self {
         Self {
             ctx_result: Ok(()),
+            dummy_host: DummyHost,
         }
     }
 }
@@ -45,71 +47,73 @@ impl SeismicHost for SeismicDummyHost {
 }
 
 impl Host for SeismicDummyHost {
-        fn basefee(&self) -> U256 {
-        U256::ZERO
+    fn basefee(&self) -> U256 {
+        self.dummy_host.basefee()
     }
 
     fn blob_gasprice(&self) -> U256 {
-        U256::ZERO
+        self.dummy_host.blob_gasprice()
     }
 
     fn gas_limit(&self) -> U256 {
-        U256::ZERO
+        self.dummy_host.gas_limit()
     }
 
     fn difficulty(&self) -> U256 {
-        U256::ZERO
+        self.dummy_host.difficulty()
     }
 
     fn prevrandao(&self) -> Option<U256> {
-        None
+        self.dummy_host.prevrandao()
     }
 
     fn block_number(&self) -> u64 {
-        0
+        self.dummy_host.block_number()
     }
 
     fn timestamp(&self) -> U256 {
-        U256::ZERO
+        self.dummy_host.timestamp()
     }
 
     fn beneficiary(&self) -> Address {
-        Address::ZERO
+        self.dummy_host.beneficiary()
     }
 
     fn chain_id(&self) -> U256 {
-        U256::ZERO
+        self.dummy_host.chain_id()
     }
 
     fn effective_gas_price(&self) -> U256 {
-        U256::ZERO
+        self.dummy_host.effective_gas_price()
     }
 
     fn caller(&self) -> Address {
-        Address::ZERO
+        self.dummy_host.caller()
     }
 
-    fn blob_hash(&self, _number: usize) -> Option<U256> {
-        None
+    fn blob_hash(&self, number: usize) -> Option<U256> {
+        self.dummy_host.blob_hash(number)
     }
 
     fn max_initcode_size(&self) -> usize {
-        0
+        self.dummy_host.max_initcode_size()
     }
 
-    fn block_hash(&mut self, _number: u64) -> Option<B256> {
-        None
+    fn block_hash(&mut self, number: u64) -> Option<B256> {
+        self.dummy_host.block_hash(number)
     }
 
     fn selfdestruct(
         &mut self,
-        _address: Address,
-        _target: Address,
+        address: Address,
+        target: Address,
     ) -> Option<StateLoad<SelfDestructResult>> {
-        None
+        self.dummy_host.selfdestruct(address, target)
     }
 
-    fn log(&mut self, _log: Log) {}
+    fn log(&mut self, _log: Log) {
+        self.dummy_host.log(_log)
+    }
 
     fn cstore(
         &mut self,
@@ -122,40 +126,42 @@ impl Host for SeismicDummyHost {
 
     fn sstore(
         &mut self,
-        _address: Address,
-        _key: U256,
-        _value: U256,
+        address: Address,
+        key: U256,
+        value: U256,
     ) -> Option<StateLoad<SStoreResult>> {
-        None
+       self.dummy_host.sstore(address, key, value)
     }
 
-    fn sload(&mut self, _address: Address, _key: U256) -> Option<StateLoad<U256>> {
-        None
+    fn sload(&mut self, address: Address, key: U256) -> Option<StateLoad<U256>> {
+       self.dummy_host.sload(address, key)
     }
 
     fn cload(&mut self, _address: Address, _key: U256) -> Option<StateLoad<U256>> {
         None
     }
 
-    fn tstore(&mut self, _address: Address, _key: U256, _value: U256) {}
-
-    fn tload(&mut self, _address: Address, _key: U256) -> U256 {
-        U256::ZERO
+    fn tstore(&mut self, address: Address, key: U256, value: U256) {
+        self.dummy_host.tstore(address, key, value)
     }
 
-    fn balance(&mut self, _address: Address) -> Option<StateLoad<U256>> {
-        None
+    fn tload(&mut self, address: Address, key: U256) -> U256 {
+        self.dummy_host.tload(address, key)
     }
 
-    fn load_account_delegated(&mut self, _address: Address) -> Option<StateLoad<AccountLoad>> {
-        None
+    fn balance(&mut self, address: Address) -> Option<StateLoad<U256>> {
+       self.dummy_host.balance(address)
     }
 
-    fn load_account_code(&mut self, _address: Address) -> Option<StateLoad<Bytes>> {
-        None
+    fn load_account_delegated(&mut self, address: Address) -> Option<StateLoad<AccountLoad>> {
+       self.dummy_host.load_account_delegated(address)
     }
 
-    fn load_account_code_hash(&mut self, _address: Address) -> Option<StateLoad<B256>> {
-        None
+    fn load_account_code(&mut self, address: Address) -> Option<StateLoad<Bytes>> {
+       self.dummy_host.load_account_code(address)
+    }
+
+    fn load_account_code_hash(&mut self, address: Address) -> Option<StateLoad<B256>> {
+       self.dummy_host.load_account_code_hash(address)
     }
 }
