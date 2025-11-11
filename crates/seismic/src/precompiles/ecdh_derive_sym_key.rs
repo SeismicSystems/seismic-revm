07d9df07bdcd03ccbf98fb8d1282aa19e7760a71
daltoncoder <daltoncoder2@gmail.com>
2025-11-11 15:16:38 -0500

    update enclave dep

--- crates/seismic/src/precompiles/ecdh_derive_sym_key.rs
+++ crates/seismic/src/precompiles/ecdh_derive_sym_key.rs
@@ -3,7 +3,7 @@ use revm::precompile::{
     u64_to_address, Precompile, PrecompileError, PrecompileId, PrecompileOutput, PrecompileResult,
 };
 
-use seismic_enclave_crypto::{
+use seismic_enclave::{
     derive_aes_key, secp256k1::ecdh::SharedSecret, secp256k1::PublicKey, secp256k1::SecretKey,
 };
 
